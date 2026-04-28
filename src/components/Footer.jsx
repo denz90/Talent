99907fe23facff1268154e6629eec92c2e48fd11
denz90 <denzelobengkumi29@gmail.com>
2026-04-28 03:38:30 +0000

    Footer Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,10 +20,22 @@ const Footer = () => {
             </p>
 
             {/* Mock Social Icons */}
-            <div className="flex space-x-4">
-              <div className="w-8 h-8 rounded-full bg-white/5 hover:bg-white/20 cursor-pointer transition-colors flex items-center justify-center text-white/50 text-xs" title="Twitter">𝕏</div>
-              <div className="w-8 h-8 rounded-full bg-white/5 hover:bg-white/20 cursor-pointer transition-colors flex items-center justify-center text-white/50 text-xs" title="LinkedIn">in</div>
-              <div className="w-8 h-8 rounded-full bg-white/5 hover:bg-white/20 cursor-pointer transition-colors flex items-center justify-center text-white/50 text-xs" title="GitHub">GH</div>
+            <div className="flex gap-4 mt-12">
+            <a href="https://www.facebook.com/profile.php?id=61580819033887" className="text-white hover:text-white/70 transition-colors">
+              <Icon icon="mdi:facebook" width="24" height="24" />
+            </a>
+            <a href="https://x.com/talentoasis_?s=20" className="text-white hover:text-white/70 transition-colors">
+              <Icon icon="mdi:twitter" width="24" height="24" />
+            </a>
+            <a href="https://www.instagram.com/talentoasis_official/?hl=en" className="text-white hover:text-white/70 transition-colors">
+              <Icon icon="mdi:instagram" width="24" height="24" />
+            </a>
+            <a href="https://www.linkedin.com/company/talentoasisofficial/posts/?feedView=all" className="text-white hover:text-white/70 transition-colors">
+              <Icon icon="mdi:linkedin" width="24" height="24" />
+            </a>
+            <a href="https://www.tiktok.com/@talentoasis_official?_r=1&_t=ZS-95ssDuHSG9g" className='text-white hover:text-white/70 transition-colors'>
+              <Icon icon="ri:tiktok-fill" width="24" height="24"/>
+            </a>
             </div>
           </div>
 
@@ -80,24 +92,6 @@ const Footer = () => {
             <a href="#" className="hover:text-white transition-colors">Privacy Policy</a>
             <a href="#" className="hover:text-white transition-colors">Terms of Service</a>
             <a href="#" className="hover:text-white transition-colors">Cookie Settings</a>
-          </div>
-          
-          <div className="flex gap-4 mt-12">
-            <a href="https://www.facebook.com/profile.php?id=61580819033887" className="text-white hover:text-white/70 transition-colors">
-              <Icon icon="mdi:facebook" width="24" height="24" />
-            </a>
-            <a href="https://x.com/talentoasis_?s=20" className="text-white hover:text-white/70 transition-colors">
-              <Icon icon="mdi:twitter" width="24" height="24" />
-            </a>
-            <a href="https://www.instagram.com/talentoasis_official/?hl=en" className="text-white hover:text-white/70 transition-colors">
-              <Icon icon="mdi:instagram" width="24" height="24" />
-            </a>
-            <a href="https://www.linkedin.com/company/talentoasisofficial/posts/?feedView=all" className="text-white hover:text-white/70 transition-colors">
-              <Icon icon="mdi:linkedin" width="24" height="24" />
-            </a>
-            <a href="https://www.tiktok.com/@talentoasis_official?_r=1&_t=ZS-95ssDuHSG9g" className='text-white hover:text-white/70 transition-colors'>
-              <Icon icon="ri:tiktok-fill" width="24" height="24"/>
-            </a>
           </div>
         </div>
       </div>
