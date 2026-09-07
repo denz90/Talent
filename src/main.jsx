d0cb4ec509c30ff48417eaeedd875e721d114adf
denz90 <denzelobengkumi29@gmail.com>
2026-09-07 15:55:31 +0000

    fix: wrap Smartlook project key in quotes to resolve initialization error

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import Smartlook from 'smartlook-client'
 
-Smartlook.init(ea742f720315df03362c8d8b51bf0975f9d13f0a);
+Smartlook.init('ea742f720315df03362c8d8b51bf0975f9d13f0a');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
