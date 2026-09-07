1faf6823be9a5d8135c248bae1531f2086e6970e
denz90 <denzelobengkumi29@gmail.com>
2026-09-07 09:56:34 +0000

    feat: integrate Smartlook analytics for user session monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Smartlook } from 'smartlook-client'
+Smartlook.init('ea742f720315df03362c8d8b51bf0975f9d13f0a');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
